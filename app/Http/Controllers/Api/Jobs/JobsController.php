319b7036c7ca5e4a1339eada8a396e8fea75926d
Hamza <hamzabitup@gmail.com>
2020-01-26 14:43:59 +0100

    Add: logic to save a job

--- app/Http/Controllers/Api/Jobs/JobsController.php
+++ app/Http/Controllers/Api/Jobs/JobsController.php
@@ -6,7 +6,6 @@ use App\Job;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Http\File;
 use Illuminate\Support\Facades\Auth;
 
 class JobsController extends Controller
@@ -41,63 +40,41 @@ class JobsController extends Controller
     {
         $job = new Job;
 
-        $job->company_pitch = $request->input('companyPitch');
         $job->job_title = $request->input('jobTitle');
-        $job->job_story = $request->input('jobStory');
-        $job->male_split = $request->input('maleSplit');
-        $job->average_age = $request->input('averageAge');
-        $job->working_hours = $request->input('workingHours');
-        $job->finance_stage = $request->input('financeStage');
-        $job->amount_raised = $request->input('amountRaised');
-        $job->starting_salary = $request->input('startingSalary');
-        $job->equity_bonus = $request->input('equityBonus');
-        $job->headcount = $request->input('headcount');
-        $job->year_founded = $request->input('yearFounded');
-        $job->company_url = $request->input('companyurl');
-        $job->start_date = $request->input('start_date');
+        $job->quick_pitch = $request->input('quickPitch');
         $job->location = $request->input('location');
-        $job->linkedin_profile_url = $request->input('linkedinProfileUrl');
-        $job->angelist_profile_url = $request->input('angelistProfileUrl');
-        $job->crunchbase_profile_url = $request->input('crunchbaseProfileUrl');
+        $job->industry = $request->input('industry');
+        $job->seniority = $request->input('seniority');
+        $job->type = $request->input('type');
         $job->application_email = $request->input('applicationEmail');
         $job->application_url = $request->input('applicationUrl');
+        $job->about_the_colleagues = $request->input('aboutTheColleagues');
+        $job->why_this_role = $request->input('whyThisRole');
 
         // storing json
-        $job->communication_tools = json_encode($request->input('communicationTools'));
-        $job->tags = json_encode($request->input('tags'));
-        $job->company_achievements = json_encode($request->input('companyAchievements'));
-        $job->interview_process = json_encode($request->input('interviewProcess'));
-        $job->communication_tools = json_encode($request->input('communicationTools'));
-        $job->tags = json_encode($request->input('tags'));
-        $job->company_achievements = json_encode($request->input('companyAchievements'));
-        $job->interview_process = json_encode($request->input('interviewProcess'));
-        $job->required_skills = json_encode($request->input('requiredSkills'));
-        $job->company_values = json_encode($request->input('company_values'));
-        $job->atmosphere_characteristics = json_encode($request->input('atmosphereCharacteristics'));
-        $job->perks = json_encode($request->input('perks'));
+        $job->skills = $request->input('skills');
+        $job->tags = $request->input('tags');
+        $job->ownership_values = $request->input('ownershipValues');
+        $job->application_qualities = $request->input('applicationQualities');
 
-
-        if($request->hasFile('hiringManagersAudioFile')) {
-            $job->hiring_managers_audio_url  = $request->file('hiringManagersAudioFile')->store(
-                'companies/audio',
+        // storing files
+        if ($request->hasFile('promoPhoto')) {
+            $path  = $request->file('promoPhoto')->store(
+                'companies/jobs/promo_photo',
                 'do_spaces'
             );
+
+            $job->promo_photo_link = Storage::disk('do_spaces')->url($path);
         }
 
-        if ($request->hasFile('employerTeamOfficeImgFile')) {
-            $job->hiring_managers_audio_url  = $request->file('employerTeamOfficeImgFile')->store(
-                'companies/images/team_office',
+        if ($request->hasFile('familyPhoto')) {
+            $path  = $request->file('familyPhoto')->store(
+                'companies/jobs/family_photos',
                 'do_spaces'
             );
-        }
 
-        if ($request->hasFile('employerLogoUrl')) {
-            $job->hiring_managers_audio_url  = $request->file('employerLogoUrl')->store(
-                'companies/images/logo',
-                'do_spaces'
-            );
+            $job->family_photo = Storage::disk('do_spaces')->url($path);
         }
-
 
         $job->user_id = Auth::id();
 
