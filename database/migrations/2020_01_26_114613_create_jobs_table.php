9aca7f2afa8f4a16d6fcf7a4f75922693cd77dde
Hamza <hamzabitup@gmail.com>
2020-01-26 13:58:42 +0100

    Add: jobs table migration

---
+++ database/migrations/2020_01_26_114613_create_jobs_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('job_title');
+            $table->string('location')->nullable();
+            $table->string('industry')->nullable();
+            $table->string('seniority')->nullable();
+            $table->string('type')->nullable();
+            $table->text('quick_pitch')->nullable();
+            $table->json('tags')->nullable();
+            $table->json('skills')->nullable();
+            $table->string('application_email')->nullable();
+            $table->json('ownership_values')->nullable();
+            $table->json('application_qualities')->nullable();
+            $table->string('promo_photo_link')->nullable();
+            $table->text('about_the_colleagues')->nullable();
+            $table->text('why_this_role')->nullable();
+            $table->string('family_photo')->nullable();
+            $table->string('application_url')->nullable();
+            $table->string('appplication_email')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
