4a8df7bf69102ba97575d1d4dc8e25c9987311e3
Hamza <hamzabitup@gmail.com>
2020-02-02 20:34:50 +0100

    Add: employer jobs search filter

--- app/Http/Controllers/Api/Employer/JobsSearchController.php
+++ app/Http/Controllers/Api/Employer/JobsSearchController.php
@@ -10,6 +10,18 @@ use Illuminate\Http\Request;
 class JobsSearchController extends Controller
 {
     /**
+     * Turn a nullisht value to an empty string
+     * @param mixed $value 
+     * @return string|void 
+     */
+    protected function nullToString($value)
+    {
+        if ($value === null) {
+            return '';
+        }
+    }
+
+    /**
      * Handle the incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -17,13 +29,22 @@ class JobsSearchController extends Controller
      */
     public function __invoke(Request $request)
     {
-        // we get the category - either live or expired
-        $query = $request->input('query');
+        $query = $this->nullToString($request->input('query'));
+        $location = $this->nullToString($request->input('location'));
+        $seniority = $this->nullToString($request->input('seniority'));
+        $industry = $this->nullToString($request->input('industry'));
+        $type = $this->nullToString($request->input('type'));
+        $category = $request->input('category'); // either live or expired
 
-        $jobs = Job::search()->get();
+        // still need to include the id of the current logged in user
+        // and also add logic to know if the job has expired or not yet
+        $jobs = Job::search($query . $location . $seniority . $industry)
+            ->where('is_unexpired', $category === 'live' ? 1 : 0)
+            ->paginate(15);
 
         return response()->json([
-            'jobs' => JobResource::collection($jobs)
+            'jobs' => JobResource::collection($jobs),
+            'query' => $query
         ]);
     }
 }
